82300ee587762ef67332c07830030e2d060f503d
Jeff <jeff@jeffhq.com>
2019-07-25 15:38:19 -0500

    Update sub-header

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "JeffHQ.com",
   shortName: "Jeff Hodges",
-  description: "Portfolio // Blog",
+  description: "My Corner of the Web",
   url: "https://jeffhq.com", // Domain of your site without prefix!
   prefix: "/",
   author: "Jeff Hodges",
