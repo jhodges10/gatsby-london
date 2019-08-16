be92654ae9c91a9c6ab08af1355ea9eef71872ff
Jeff <jeff@jeffhq.com>
2019-08-15 17:40:13 -0700

    Updated favicon and icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -135,7 +135,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/jeffhq_logo.png`,
       },
     },
     `gatsby-plugin-netlify`,
